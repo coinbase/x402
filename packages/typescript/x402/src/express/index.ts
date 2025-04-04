4d91b6895c525c8811489491079e0c3a55d6d951
Quazia <alf40k@gmail.com>
2025-04-04 16:00:23 -0400

    fix: shared paywall export

--- packages/typescript/x402/src/express/index.ts
+++ packages/typescript/x402/src/express/index.ts
@@ -11,7 +11,7 @@ import { Address } from "viem";
 import { getUsdcAddressForChain } from "../shared/evm/usdc";
 import { useFacilitator } from "../client";
 import { getPaywallHtml } from "../shared/paywall";
-export { getPaywallHtml } from "./paywall";
+export { getPaywallHtml } from "../shared/paywall";
 
 interface PaymentMiddlewareOptions {
   description?: string;
