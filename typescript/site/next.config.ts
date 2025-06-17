1924a166ca390ca8729f50e041ceb8045de79956
Amanda McDermott <amanda.mcdermott@coinbase.com>
2025-06-17 18:30:48 -0400

    Update config files

--- typescript/site/next.config.ts
+++ typescript/site/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
@@ -10,13 +10,8 @@ const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
-
-    // https://github.com/webpack/webpack/issues/14798
-    // config.externals.push({
-    //   "node:crypto": "commonjs crypto",
-    // });
 
     return config;
   },
