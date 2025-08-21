005b048d3ed7262a8b6e458c295d6e5b1abc85d6
SkyYap <skky5687@gmail.com>
2025-08-22 02:02:30 +0800

    Adding Scroll and Scroll Network Support

--- typescript/packages/x402/src/types/shared/network.ts
+++ typescript/packages/x402/src/types/shared/network.ts
@@ -10,6 +10,8 @@ export const NetworkSchema = z.enum([
   "solana",
   "sei",
   "sei-testnet",
+  "scroll",
+  "scroll-sepolia",
 ]);
 export type Network = z.infer<typeof NetworkSchema>;
 
@@ -22,6 +24,8 @@ export const SupportedEVMNetworks: Network[] = [
   "iotex",
   "sei",
   "sei-testnet",
+  "scroll",
+  "scroll-sepolia",
 ];
 export const EvmNetworkToChainId = new Map<Network, number>([
   ["base-sepolia", 84532],
@@ -31,6 +35,8 @@ export const EvmNetworkToChainId = new Map<Network, number>([
   ["iotex", 4689],
   ["sei", 1329],
   ["sei-testnet", 1328],
+  ["scroll", 534352],
+  ["scroll-sepolia", 534351],
 ]);
 
 // svm
