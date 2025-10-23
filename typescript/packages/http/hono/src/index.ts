a32db807526f228ef63d87d493b8e935509ef492
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-10-23 11:07:23 -0700

    scaffold: svm, axios, hono, next, sign-in-with-x & paywall

---
+++ typescript/packages/http/hono/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @x402/hono - x402 Payment Protocol Hono Extension
+ * This module provides Hono middleware for handling x402 payment required responses.
+ */
+
+export { };
---
+++ typescript/packages/http/next/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @x402/next - x402 Payment Protocol Next.js Extension
+ * This module provides Next.js middleware for handling x402 payment required responses.
+ */
+
+export { };
