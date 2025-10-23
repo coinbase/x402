d1824aa8ba3a2ec77e3a093618d40edc851be8da
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-10-23 11:07:23 -0700

    scaffold: svm, axios, hono, next, sign-in-with-x & paywall

---
+++ typescript/packages/http/paywall/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @x402/paywall - x402 Payment Protocol Paywall Extension
+ * This module provides paywall functionality for the x402 payment protocol.
+ */
+
+export { };
---
+++ typescript/packages/mechanisms/svm/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @x402/svm - x402 Payment Protocol SVM Implementation
+ *
+ * This module provides the SVM-specific implementation of the x402 payment protocol.
+ */
+
+export { };
