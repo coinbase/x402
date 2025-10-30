65880938efaaf5fd7e3be5fcf7287cb561060563
Amanda McDermott <amanda.mcdermott@coinbase.com>
2025-10-30 17:29:55 -0400

    chore(paywall): fix formatting issues
    - Run prettier on all paywall files
    - Fix code style across 15 files
    - No functional changes

--- typescript/packages/http/paywall/eslint.config.js
+++ typescript/packages/http/paywall/eslint.config.js
@@ -3,6 +3,14 @@ import baseConfig from "../../../eslint.config.js";
 export default [
   ...baseConfig,
   {
-    ignores: ["dist/**", "src/gen/**", "src/dist/**", "src/evm/gen/**", "src/evm/dist/**", "src/svm/gen/**", "src/svm/dist/**"],
+    ignores: [
+      "dist/**",
+      "src/gen/**",
+      "src/dist/**",
+      "src/evm/gen/**",
+      "src/evm/dist/**",
+      "src/svm/gen/**",
+      "src/svm/dist/**",
+    ],
   },
 ];
