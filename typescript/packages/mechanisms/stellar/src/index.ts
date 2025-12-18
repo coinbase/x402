05f9648f978e86228a9362b08037cb3f6e3157c8
Marcelo Salloum <marcelosalloum@gmail.com>
2025-12-18 12:22:18 -0800

    Add stellar-specific utils and constants, like getRpc, USDC_PUBNET_ADDRESS, regexes, etc that will be used across multiple Stellar implementations.

---
+++ typescript/packages/mechanisms/stellar/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @module @x402/stellar - x402 Payment Protocol Stellar Implementation
+ *
+ * This module provides the Stellar-specific implementation of the x402 payment protocol.
+ */
+
+// Export constants
+export * from "./constants";
+
+// Export utils
+export * from "./utils";
